e58a895c8a493d8557cbda2ad3cc07b4fa0b8803
Eric Souza <eric.bh18souza@gmail.com>
2022-01-09 10:43:02 -0300

    feat: :sparkles: Static props and input types developed

---
+++ src/state/UserSearch.tsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+
+const users = [
+  { name: 'Sarah', age: 20 },
+  { name: 'Alex', age: 25 },
+  { name: 'Michael', age: 28 },
+];
+
+type FoundUser = { name: string; age: number };
+
+const UserSearch: React.FC = () => {
+  const [name, setName] = useState('');
+  const [user, setUser] = useState<FoundUser | undefined>();
+
+  const onClick = () => {
+    const foundUser = users.find((user) => {
+      return user.name === name;
+    });
+
+    setUser(foundUser);
+  };
+
+  return (
+    <div>
+      User search
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={onClick}>Search user</button>
+      <div>
+        {user && user.name}
+        {user && user.age}
+      </div>
+    </div>
+  );
+};
+
+export default UserSearch;
